8bdcfa4b93759276ff348d82170450af88ef4a57
nasyaoy <122662286+nasyaoy@users.noreply.github.com>
2024-02-24 14:45:20 +0500

    практическая работа

---
+++ script.js
@@ -0,0 +1,8 @@
+// 1
+let cities = document.querySelectorAll('li');
+cities = JSON.stringify(cities);
+console.log(cities);
+
+//2 
+let json = '[user1, user2, user3, user4, user5]';
+// превратитть в масив и вернтть обратно
